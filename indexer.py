c50f05cf79ff4287bfe52ce8e39bc012b2c33874
Dorizzz0610 <lzhangcx@connect.ust.hk>
2023-04-11 22:25:36 +0800

    Modify indexing approach

--- indexer.py
+++ indexer.py
@@ -13,6 +13,26 @@ import stemmer
 # - The indexes must be able to support phrase search such as “hong kong” in page titles and page bodies.
 
 
-def indexing(url, max_pages):
+def indexing(keyword_index, title_index, url, max_pages):
+    # indexing the keywords and body of a set of crawled pages
+    
     crawled_result = crawler.crawl(url, max_pages)
+    
+    
+    for url, page in crawled_result.items():
+        title = page["title"]
+        body = page["keywords"]
+        
+        title = stopwords_remover.remove_stopwords(title)
+        body = stopwords_remover.remove_stopwords(body)
 
+        title = stemmer.stemming(title)
+        body = stemmer.stemming(body)
+
+        title = list(set(title))
+        body = list(set(body))
+
+        title_index[url] = title
+        keyword_index[url] = body
+
+    return keyword_index, title_index
--- web_interface/src/index.js
+++ web_interface/src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
-import userInput from './userInput'
+import searchBox from './searchBox'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <App />
-    <userInput />
+    <searchBox />
   </React.StrictMode>
 )
 
