cfe22a3a27b29e1a8c33642cf5ab670a150cd15d
Dorizzz0610 <lzhangcx@connect.ust.hk>
2023-04-14 16:37:59 +0800

    Modify main functions in searcher

--- backend/searcher.py
+++ backend/searcher.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 import math
 from scipy.spatial.distance import cosine
+from sklearn.metrics.pairwise import cosine_similarity
 
 
 
@@ -16,59 +17,49 @@ def calculate_tfxidf(inverted_index, size):
     # idf: a dictionary. key: term, value: idf value of the term
     idf = {term: math.log2(size/df[term]) for term in df}
 
+    max_tf = 0
+    for doc_freq in inverted_index.values():
+        if(max_tf < doc_freq[1]): max_tf = doc_freq[1]
 
-    # tf_idf_weights: a dictionary. key: term, value: dictionary,  {doc_id: tf-idf weight}
+    # tf_idf_weights: a dictionary. key: term, value: dictionary,  {doc_id: tf-idf_weight}
+    # tf-idf weight is calculated by: tfxidf/max(tf) 
     tf_idf_weights = {}
     for term, doc_freq in inverted_index.items():
         for doc_id, tf in doc_freq:
-            tf_idf_weights.setdefault(term, {})[doc_id] = (1 + math.log2(tf)) * idf[term]
+            tf_idf_weights.setdefault(term, {}).update({doc_id: (1 + math.log2(tf)) * idf[term]}/max_tf)
 
     return tf_idf_weights
 
 
 
+
 def calculate_similarity(query, keyword_weights, title_weights, FAVOR):
-    # weights: a dictionary. key: term, value: dictionary,  {doc_id: (tf-idf weight, term frequency)}
+    # tf_idf_weights: a dictionary. key: term, value: dictionary,  {doc_id: tf-idf_weight}
 
-    similarity = {} # default dict is not hashable! so we use another way to initialize here
-    for doc in keyword_weights.values():
-        for doc_id in doc.keys():
-            similarity[doc_id] = 0 # dict isn't hashable
-
+    all_terms = set(keyword_weights.keys()).union(set(title_weights.keys())).union(set(query))
     
-    query_vector = []
-    for word in query:
-        keyword_weight = 0
-        title_weight = 0
-        if word in keyword_weights.keys():
-            keyword_weight = keyword_weights[word]
-        # if word in title_weights.keys():
-        #     title_weight = FAVOR * title_weights[word]
-        for word, weights in title_weights.items():
-            for doc_id, weight in weights.items():
-                keyword_weight
-                
-        combined_weight = {doc_id: (keyword_weight.get(doc_id, 0)[0] + title_weight.get(doc_id, 0)[0]) for doc_id in similarity.keys()} # get(doc_id, 0) means that the default value of doc_id is 0
-        query_vector.append(combined_weight)
-
-
-    # iterate each document and generate a document vector
-    for doc_id in similarity.keys():
-        doc_vector = []
-        for word in query:
-            keyword_weight = 0
-            title_weight = 0
-            if word in keyword_weights.keys():
-                keyword_weight = keyword_weights[word].get(doc_id, (0, 0))[0]
-            if word in title_weights.keys():
-                title_weight = FAVOR * title_weights[word].get(doc_id, (0, 0))[0]
-            doc_vector.append(keyword_weight + title_weight)
-
-        similarity[doc_id] = 1 - cosine(list(query_vector[i][doc_id] for i in range(len(query))), doc_vector)
-
-
-    # simialrity: a dictionary. key: doc_id, value: cosine similarity
-    return similarity
+    query_vector = {term: 0 for term in all_terms}
+    for term in query:
+        query_vector[term] = 1
+    
+    doc_vectors = []
+    for doc_id in keyword_weights.keys():
+        doc_vectors[doc_id] = {term: 0 for term in all_terms}
+        for term in all_terms:
+            if term in keyword_weights.keys():
+                doc_vectors[doc_id][term] += keyword_weights[term][doc_id]
+            if term in title_weights.keys():
+                doc_vectors[doc_id][term] += title_weights[term][doc_id] * FAVOR
+            if term not in keyword_weights.keys() and term not in title_weights.keys():
+                doc_vectors[doc_id][term] = 0
+        
+    
+    docs_similarity = []
+    for doc_id in range(len(doc_vectors)):
+        score = cosine_similarity(query_vector, doc_vectors[doc_id])
+        docs_similarity[doc_id] = score
+        
+    return docs_similarity
 
 
 
@@ -81,19 +72,21 @@ def retrieval_function(query, keyword_index, title_index, max_pages):
     
     FAVOR = 1.5 # a constant to boost the rank of a page if there is a match in the title
 
+
     # the weights are nested dictionary containing weights
     # outer dictionary - key: term  value: inner dictionary
     # inner dictionary - key: doc_id value: tfxidf weight
     keyword_weights = calculate_tfxidf(keyword_index, max_pages)
     title_weights = calculate_tfxidf(title_index, max_pages)
 
-    similarity = calculate_similarity(query, keyword_weights, title_weights, FAVOR)
+    # docs_similarity: a list - index: doc_id, value: cosine similarity
+    docs_similarity = calculate_similarity(query, keyword_weights, title_weights, FAVOR)
 
 
     top_doc = 0 # page_id of the top document
 
-    for doc_id in similarity.keys(): 
-        if similarity[doc_id] > similarity[top_doc]: 
+    for doc_id in range(len(docs_similarity)): 
+        if docs_similarity[doc_id] > docs_similarity[top_doc]: 
             top_doc = doc_id 
 
 
