1e0799155aaaca3a9c45c25eccfd0e67e6da1d2d
Dorizzz0610 <lzhangcx@connect.ust.hk>
2023-05-06 21:49:27 +0800

    Delete position in query, instead store single terms and phrases directly

--- backend/main.py
+++ backend/main.py
@@ -1,38 +1,47 @@
-from collections import defaultdict
 import indexer
 import crawler
 import searcher
 
 
 def handle_query(query):
-    words = query.split()
+    single_terms = []
+    phrases = []
 
     in_phrase = False
-    phrase = ""
-    new_words = []
-    phrase_position = []
-    new_phrase_position = [] # elements: tuple (begin, end, index), index is the position of the phrase in new_words
+    current_phrase = ""
+    current_term = ""
 
-    for i, word in enumerate(words):
-        if word.startswith('"'):
-            in_phrase = True
-            phrase = word[1:]
-            phrase_position.append((i,))
-        elif in_phrase:
-            phrase += " " + word
-            if word.endswith('"'):
-                new_words.append(phrase[:-1])
-                start = phrase_position[-1][0]
-                end = i
-                phrase_position[-1] = (start, end)
-                new_phrase_position.append((start, end, len(new_words)-1))
+    for i in query:
+        if i == '"':
+            if in_phrase:
+                phrases.append(current_phrase)
+                current_phrase = ""
                 in_phrase = False
             else:
-                phrase_position[-1] = (phrase_position[-1][0], i)
+                in_phrase = True
+                if current_term:
+                    single_terms.append(current_term)
+                    current_term = ""
+        elif i == " ":
+            if in_phrase:
+                current_phrase += i
+            else:
+                if current_term:
+                    single_terms.append(current_term)
+                    current_term = ""
         else:
-            new_words.append(word)
+            if in_phrase:
+                current_phrase += i
+            else:
+                current_term += i
 
-    return new_words, new_phrase_position
+    if current_term:
+        single_terms.append(current_term)
+    if current_phrase:
+        phrases.append(current_phrase)
+
+
+    return single_terms, phrases
 
 
 def return_to_frontend(crawled_result, search_result):
@@ -50,33 +59,31 @@ def return_to_frontend(crawled_result, search_result):
 
 def main():
     starting_url = "https://cse.hkust.edu.hk"
-    MAX_PAGES = 10
+    MAX_PAGES = 30
     
     # Step 0: Get the query
     query = input("Enter your query: ")
-    query, query_phrase_position = handle_query(query)
+    query_term, query_phrase = handle_query(query)
 
-    # query is now a list. Each element is either a word or a phrase
-    print(query)
-    print(query_phrase_position)
+    print(query_term)
+    print(query_phrase)
 
 
     # Step 1: Crawl the pages
     print("Crawling the pages...")
-    crawled_result = crawler.crawl(starting_url, MAX_PAGES)
+    crawled_result = crawler.crawl(starting_url, MAX_PAGES) # store based on id 
 
 
     # Step 2: Index the crawled pages
     print("Indexing the pages...")
-    body_index = defaultdict(list)
-    title_index = defaultdict(list)
 
-    body_index, title_index = indexer.indexing(query, query_phrase_position, body_index, title_index, crawled_result)
+
+    body_index, title_index = indexer.indexing(crawled_result)
 
 
     # Step 3: Search the query
     print("Searching the query...")
-    search_result = searcher.retrieval_function(query, body_index, title_index, MAX_PAGES, 1.5)
+    search_result = searcher.retrieval_function(query_term, query_phrase, body_index, title_index, MAX_PAGES, 1.5)
     
 
 
