eab8eb6707c912e73ab261b90305e4c3144ef62e
Dorizzz0610 <lzhangcx@connect.ust.hk>
2023-05-09 23:32:53 +0800

    Environment updates

--- backend/connect_frontend.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, request
-
-app = Flask(__name__)
-
-@app.route('/search', methods=['POST'])
-def search():
-    search_text = request.json.get('searchText')
-    return search_text
